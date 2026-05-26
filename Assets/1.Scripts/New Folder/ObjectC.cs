5b242ae5558ab1d0970840d970b54719e6b0b8a8
신영준 <kdrsa789@gmail.com>
2026-05-26 14:32:05 +0900

    인터페이스 상속받는 오브젝트 C

---
+++ Assets/1.Scripts/New Folder/ObjectC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ObjectC : MonoBehaviour, IObjectable
+{
+
+    [SerializeField] private string _objectName;
+    public string ObjectName { get { return _objectName; } set { _objectName = value; } }
+
+    [SerializeField] private int _objectNumber;
+    public int ObjectNumber { get { return _objectNumber; } set { _objectNumber = value; } }
+
+    public void PrintSomthing()
+    {
+        Debug.Log("오브젝트 C에서 아무거나 출력");
+    }
+
+    public void SetTextMeshNameOnInit()
+    {
+    }
+}
