7b6272a2479e562313020141dfec673901f21c86
신영준 <kdrsa789@gmail.com>
2026-05-26 15:18:19 +0900

    오브젝트 삭게 유틸 컴포넌트

---
+++ Assets/1.Scripts/Common/GameUtilExtension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameUtilExtension // 오브젝트 삭제 컴포넌트
+{
+    public static void RemoveComponent<T>(this GameObject obj) where T : Component
+    {
+        T component = obj.GetComponent<T>();
+        if (component != null)
+        {
+            Debug.LogWarning($"{obj.name} 오브젝트에서 {typeof(T).Name} 컴포넌트가 제거되었습니다.");
+            Object.Destroy(component);
+        }
+    }
+}
