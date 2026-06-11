71424868fe9b11ba89fade011cfb735b39bfcb54
신영준 <kdrsa789@gmail.com>
2026-06-11 21:52:15 +0900

    C# 을 이용한 프로젝트 전체의 공용 함수 기본 골자 (프린트 출력 함수)

---
+++ Assets/1.Scripts/01.BasicCodeRef/StaticClass.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+// C# 을 이용한 프로젝트 전체의 공용 함수 샘플
+public static class StaticClass
+{
+    // 프린트 출력 함수
+    public static void PrintStaticText()
+    {
+        Debug.Log("스태틱 클래스로 스태틱 메서드인 출력함수도 호출해보기");
+    }
+}
