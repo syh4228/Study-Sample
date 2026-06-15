5055ac1d3253ca174503dd83457f2c3ed99bad2f
신영준 <kdrsa789@gmail.com>
2026-06-15 23:37:14 +0900

    객체지향 프로그래밍의 핵심 개념인 '상속'을 보여주는 뼈대(구조) 코드

---
+++ Assets/1.Scripts/02.2D/FieldObject_2D.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// 객체지향 프로그래밍의 핵심 개념인 '상속(Inheritance)'을 보여주는 중요한 뼈대(구조) 코드
+// 클래스 이름 뒤에 콜론(:)을 붙이고 다른 클래스 이름을 적음
+public class FieldObject_2D : FieldObject
+{
+
+}
